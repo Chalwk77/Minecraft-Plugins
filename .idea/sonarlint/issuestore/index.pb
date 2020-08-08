dfc414ceae89d59d7756c18082cfc285f431a242
jericho <jericho.crosby227@gmail.com>
2020-08-08 19:28:29 +1200

    Initial Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+i
+9MVPJoinAlerts/src/main/java/jericraft/chalwk/Message.java,0\7\075e45f48af0a79694f741ea4b2791d36d17e1f1
+]
+-iSpy/src/main/java/jericraft/chalwk/ispy.java,6\5\6549c84ba313bdfe1c7f60ee73895c245be70ea9
+f
+6MVPJoinAlerts/src/main/java/jericraft/chalwk/main.java,f\b\fbb9884177ae3ad76b01fdbb77de40bd850caa53
+O
+MVPJoinAlerts/MVPJoinAlerts.iml,4\6\46baf58b958c2c5925cf777d841bdf2ecd44511d
+m
+=Health Regeneration/src/main/java/jericraft/chalwk/Timer.java,6\0\60f74bdb1e65a6cad1471c8b7e1755250ab69b44
+l
+<Health Regeneration/src/main/java/jericraft/chalwk/main.java,c\4\c4341cc0cf660166a976f8c0e1cc0563904f6883
---
+++ Health Regeneration/src/main/java/jericraft/chalwk/main.java
@@ -0,0 +1,42 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package jericraft.chalwk;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener {
+
+    int rate = this.getConfig().getInt("rate");
+    int delay = this.getConfig().getInt("delay");
+
+    public void onEnable() {
+        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "+----------------------------------------+");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "|       " + ChatColor.WHITE + "Health Regen " + this.getDescription().getVersion() + " - Enabled" + ChatColor.GREEN + "       |");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "+----------------------------------------+");
+        this.getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+
+            for (Player p : this.getServer().getOnlinePlayers()) {
+                if (p.getHealth() != 20.0D) {
+                    if (p.getHealth() + (double) this.rate > 20.0D) {
+                        p.setHealth(20.0D);
+                    } else {
+                        p.setHealth(p.getHealth() + (double) this.rate);
+                    }
+                }
+            }
+
+        }, 0, delay * 20);
+    }
+
+    public void onDisable() {
+        this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "+----------------------------------------+");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "|       " + ChatColor.WHITE + "Health Regen " + this.getDescription().getVersion() + " - Disabled" + ChatColor.RED + "      |");
+        this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "+----------------------------------------+");
+    }
+}
