b8523bfe71284b299fef96a99acac1668a878501
jericho <jericho.crosby227@gmail.com>
2020-12-22 15:47:18 +1300

    Initial Commit

--- EnjinWebstorePerks/src/main/java/jericraft/chalwk/CommandClass.java
+++ EnjinWebstorePerks/src/main/java/jericraft/chalwk/CommandClass.java
@@ -21,45 +21,65 @@ import java.util.Map;
 public class CommandClass extends BukkitCommand implements Listener {
 
     private final FileConfiguration config;
+
     public CommandClass(String cmd, EnjinWebstorePerks plugin) {
 
         super(cmd);
         this.description = "View all purchased webstore perks";
         this.usageMessage = "/" + cmd;
         this.config = plugin.getConfig();
-
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
     public boolean execute(CommandSender sender, String alias, String[] args) {
-        if (sender instanceof Player) {
+
+        if (args.length >= 1) {
+            if (sender.hasPermission(config.getString("RELOAD_PERMISSION"))) {
+                if (args[0].equalsIgnoreCase("reload")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&m==========================="));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("ONRELOAD")));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&m==========================="));
+                    EnjinWebstorePerks.ReloadConfig();
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Invalid Command Syntax"));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Usage: &8Usage: /" + config.getString("commands") + " reload"));
+                }
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("NO_PERMISSION")));
+            }
+        } else if (sender instanceof Player) {
 
             //Player p = (Player) sender;
             //PermissionUser user = PermissionsEx.getUser(p.getName());
             //List<String> perms = user.getOwnPermissions(p.getWorld().getName());
 
             int total = 0;
-            sender.sendMessage(ChatColor.GREEN + "========== [PERKS] ==========");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("HEADER")));
+            String content = config.getString("CONTENT");
+
             for (Map<?, ?> opt : config.getMapList("nodes")) {
                 List<String> node = new ArrayList<>((Collection<? extends String>) opt.keySet());
                 for (String s : node) {
                     if (sender.hasPermission(s)) {
                         List<String> perks = new ArrayList<>((Collection<? extends String>) opt.values());
-                        sender.sendMessage(ChatColor.AQUA + perks.get(0));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', content.replace("%perks%", perks.get(0))));
                         total = total + Integer.parseInt(perks.get(1));
                     }
                 }
             }
 
             if (total > 0) {
-                sender.sendMessage(ChatColor.GREEN + "Total amount of money spend: $" + total);
+                String footer = config.getString("FOOTER");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', footer.replace("%total%", Integer.toString(total))));
             } else {
-                sender.sendMessage(ChatColor.RED + "You have not purchased any perks!");
+                String str = config.getString("NO_PURCHASES");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', str));
             }
         } else {
             sender.sendMessage("Only players can execute that command!");
         }
+
         return true;
     }
 }
--- EnjinWebstorePerks/src/main/java/jericraft/chalwk/EnjinWebstorePerks.java
+++ EnjinWebstorePerks/src/main/java/jericraft/chalwk/EnjinWebstorePerks.java
@@ -4,7 +4,6 @@
 package jericraft.chalwk;
 
 import org.bukkit.command.CommandMap;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,21 +11,26 @@ import java.lang.reflect.Field;
 
 public final class EnjinWebstorePerks extends JavaPlugin implements Listener {
 
-    public static FileConfiguration config;
+    public static EnjinWebstorePerks instance;
 
     @Override
     public void onEnable() {
+
+        instance = this;
         this.saveDefaultConfig();
-        config = this.getConfig();
         this.getServer().getPluginManager().registerEvents(this, this);
         try {
             final Field bukkitCommandMap = this.getServer().getClass().getDeclaredField("commandMap");
             bukkitCommandMap.setAccessible(true);
             CommandMap commandMap = (CommandMap) bukkitCommandMap.get(this.getServer());
-            String cmd = config.getString("command");
+            String cmd = this.getConfig().getString("command");
             commandMap.register(cmd, new CommandClass(cmd, this));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void ReloadConfig() {
+        instance.reloadConfig();
     }
 }
